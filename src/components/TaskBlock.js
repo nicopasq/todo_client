c608ebca399fba5ae5c1712bf819e0fb9baa45ec
Nico <nicopasquariello@gmail.com>
2023-06-23 12:54:50 -0600

    Added css to all components.

--- src/components/TaskBlock.js
+++ src/components/TaskBlock.js
@@ -1,36 +1,46 @@
 // Rectangular block that will display the tasks
 // Each task will be able to be completed or deleted
-// Each tasks will be editable 
+// Each tasks will be editable
 // Tasks come from fetch request to API
 import React from "react";
-import '../styles/taskBlock.css'
+import "../styles/taskBlock.css";
 import { Button, Checkbox, Grid, Typography } from "@mui/joy";
 
-function TaskBlock({taskList}){
-    // const tasks = taskList.map(task => task)
-    const tasks = taskList.map((task, id) => (
-        <li key={id} className="task">
-            <Grid id="gridContainer" container spacing={4} sx={{flexGrow: 1, width:"100%"}}>
-                <Grid xs={2}>
-                <Typography id="desc" level="h2">{task.description}</Typography>
-                </Grid>
-                <Grid xs={8}>
-                <Typography id="category" level="h2">{task.category}</Typography>
-                </Grid>
-                <Grid xs={2}>
-                <Checkbox id="checkBox" checked={task.completed}/>
-                <Button variant="plain" sx={{margin:'10px', top:'-0.5vh'}}>Delete</Button>
-                </Grid>
-            </Grid>
-        </li>
-    ))
-    return (
-        <div id="taskBlock">
-            <ul style={{listStyleType: "none"}}>
-            {tasks}
-            </ul>
-        </div>
-        )
-    }
-    
-    export default TaskBlock;
+function TaskBlock({ taskList }) {
+  const tasks = taskList.map((task, id) => (
+    <li key={id} className="task">
+      <Grid
+        id="gridContainer"
+        container
+        spacing={4}
+        sx={{ flexGrow: 1, width: "100%" }}
+      >
+        <Grid xs={4}>
+          <Typography color="warning" id="desc" level="h2">
+            {task.description}
+          </Typography>
+        </Grid>
+        <Grid xs={4}>
+          <Typography color="info" id="category" level="h2">
+            {task.category}
+          </Typography>
+        </Grid>
+        <Grid xs={2}>
+            <div style={{float:"right", marginLeft:"46px"}}>
+          <Checkbox id="checkBox" checked={task.completed} />
+          <Button variant="plain" sx={{ margin: "10px", top: "-0.5vh" }}>
+            Delete
+          </Button>
+            </div>
+        </Grid>
+      </Grid>
+    </li>
+  ));
+  return (
+    <div id="taskBlock">
+      <ul style={{ listStyleType: "none" }}>{tasks}</ul>
+    </div>
+  );
+}
+
+export default TaskBlock;
