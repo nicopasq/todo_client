4de59af64a2e1c31a8d967bd89627d4f85b71d38
Nico <nicopasquariello@gmail.com>
2023-06-23 13:35:27 -0600

    When a new task is submitted, an object is created that will be used to create the new task.

--- src/components/CreateTask.js
+++ src/components/CreateTask.js
@@ -3,10 +3,20 @@ import React, { useState } from "react";
 import "../styles/createTask.css";
 
 function CreateTask() {
-    const [desc, setDesc] = useState('')
-    const [cat, setCat] = useState('')
+    const [newTask, setNewTask] = useState({
+        description:'',
+        category:'',
+        completed:false
+    })
+    const {category, description} = newTask
+
     function handleSubmit(e){
         e.preventDefault()
+        console.log(newTask)
+    }
+
+    function handleChange(e){
+        setNewTask({...newTask, [e.target.name]: e.target.value})
     }
 
   return (
@@ -17,16 +27,18 @@ function CreateTask() {
             <Textarea
             className="textInput"
             placeholder="Description of Task"
-            onChange={e => setDesc(e.target.value)}
-            value={desc}
+            onChange={e => handleChange(e)}
+            value={description}
+            name="description"
             />
           </Grid>
           <Grid xs={4}>
             <Textarea
             className="textInput"
             placeholder="Category" 
-            onChange={e => setCat(e.target.value)}
-            value={cat}
+            onChange={e => handleChange(e)}
+            value={category}
+            name="category"
             />
           </Grid>
           <Grid xs={4}>
